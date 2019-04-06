4cc92a681c550e97219c30c7934bdb1f8638ad02
Moriaty <sj86033503@gmail.com>
2019-04-06 10:13:12 +0200

    dubugging resnet

--- src/retinaNN_training.py
+++ src/retinaNN_training.py
@@ -75,69 +75,69 @@ def get_unet(n_ch,patch_height,patch_width):
 #Define the neural network resnet-50
 #you need change function call "get_unet" to "get_resnet" in line 226 before use this network
 
-# def Conv2d_BN(x, nb_filter, kernel_size, strides=(1, 1), padding='same', name=None):
-#     if name is not None:
-#         bn_name = name + '_bn'
-#         conv_name = name + '_conv'
-#     else:
-#         bn_name = None
-#         conv_name = None
+def Conv2d_BN(x, nb_filter, kernel_size, strides=(1, 1), padding='same', name=None):
+    if name is not None:
+        bn_name = name + '_bn'
+        conv_name = name + '_conv'
+    else:
+        bn_name = None
+        conv_name = None
 
-#     x = Conv2D(nb_filter, kernel_size, padding=padding, strides=strides, activation='relu', name=conv_name)(x)
-#     x = BatchNormalization(axis=3, name=bn_name)(x)
-#     return x
-
-
-# def bottleneck_Block(inpt,nb_filters,strides=(1,1),with_conv_shortcut=False):
-#     k1,k2,k3=nb_filters
-#     x = Conv2d_BN(inpt, nb_filter=k1, kernel_size=1, strides=strides, padding='same')
-#     x = Conv2d_BN(x, nb_filter=k2, kernel_size=3, padding='same')
-#     x = Conv2d_BN(x, nb_filter=k3, kernel_size=1, padding='same')
-#     if with_conv_shortcut:
-#         shortcut = Conv2d_BN(inpt, nb_filter=k3, strides=strides, kernel_size=1)
-#         x = add([x, shortcut])
-#         return x
-#     else:
-#         x = add([x, inpt])
-#         return x
+    x = Conv2D(nb_filter, kernel_size, padding=padding, strides=strides, activation='relu', name=conv_name)(x)
+    x = BatchNormalization(axis=3, name=bn_name)(x)
+    return x
 
 
-# def resnet_50(channel,height,width,classes=20):
-#     inpt = Input(shape=(width, height, channel))
-#     x = ZeroPadding2D((3, 3))(inpt)
-#     x = Conv2d_BN(x, nb_filter=64, kernel_size=(7, 7), strides=(2, 2), padding='valid')
-#     x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+def bottleneck_Block(inpt,nb_filters,strides=(1,1),with_conv_shortcut=False):
+    k1,k2,k3=nb_filters
+    x = Conv2d_BN(inpt, nb_filter=k1, kernel_size=1, strides=strides, padding='same')
+    x = Conv2d_BN(x, nb_filter=k2, kernel_size=3, padding='same')
+    x = Conv2d_BN(x, nb_filter=k3, kernel_size=1, padding='same')
+    if with_conv_shortcut:
+        shortcut = Conv2d_BN(inpt, nb_filter=k3, strides=strides, kernel_size=1)
+        x = add([x, shortcut])
+        return x
+    else:
+        x = add([x, inpt])
+        return x
 
-#     #conv2_x
-#     x = bottleneck_Block(x, nb_filters=[64,64,256],strides=(1,1),with_conv_shortcut=True)
-#     x = bottleneck_Block(x, nb_filters=[64,64,256])
-#     x = bottleneck_Block(x, nb_filters=[64,64,256])
 
-#     #conv3_x
-#     x = bottleneck_Block(x, nb_filters=[128, 128, 512],strides=(2,2),with_conv_shortcut=True)
-#     x = bottleneck_Block(x, nb_filters=[128, 128, 512])
-#     x = bottleneck_Block(x, nb_filters=[128, 128, 512])
-#     x = bottleneck_Block(x, nb_filters=[128, 128, 512])
+def resnet_50(channel,height,width,classes=20):
+    inpt = Input(shape=(width, height, channel))
+    x = ZeroPadding2D((3, 3))(inpt)
+    x = Conv2d_BN(x, nb_filter=64, kernel_size=(7, 7), strides=(2, 2), padding='valid')
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
 
-#     #conv4_x
-#     x = bottleneck_Block(x, nb_filters=[256, 256, 1024],strides=(2,2),with_conv_shortcut=True)
-#     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-#     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-#     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-#     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
-#     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
+    #conv2_x
+    x = bottleneck_Block(x, nb_filters=[64,64,256],strides=(1,1),with_conv_shortcut=True)
+    x = bottleneck_Block(x, nb_filters=[64,64,256])
+    x = bottleneck_Block(x, nb_filters=[64,64,256])
 
-#     #conv5_x
-#     x = bottleneck_Block(x, nb_filters=[512, 512, 2048], strides=(2, 2), with_conv_shortcut=True)
-#     x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
-#     x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
+    #conv3_x
+    x = bottleneck_Block(x, nb_filters=[128, 128, 512],strides=(2,2),with_conv_shortcut=True)
+    x = bottleneck_Block(x, nb_filters=[128, 128, 512])
+    x = bottleneck_Block(x, nb_filters=[128, 128, 512])
+    x = bottleneck_Block(x, nb_filters=[128, 128, 512])
 
-#     x = AveragePooling2D(pool_size=(7, 7))(x)
-#     x = Flatten()(x)
-#     x = Dense(classes, activation='softmax')(x)
+    #conv4_x
+    x = bottleneck_Block(x, nb_filters=[256, 256, 1024],strides=(2,2),with_conv_shortcut=True)
+    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
+    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
+    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
+    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
+    x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
 
-#     model = Model(inputs=inpt, outputs=x)
-#     return model
+    #conv5_x
+    x = bottleneck_Block(x, nb_filters=[512, 512, 2048], strides=(2, 2), with_conv_shortcut=True)
+    x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
+    x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
+
+    x = AveragePooling2D(pool_size=(7, 7))(x)
+    x = Flatten()(x)
+    x = Dense(classes, activation='softmax')(x)
+
+    model = Model(inputs=inpt, outputs=x)
+    return model
 
 #==================================================
 
@@ -237,8 +237,8 @@ visualize(group_images(patches_masks_train[0:N_sample,:,:,:],5),'./'+name_experi
 n_ch = patches_imgs_train.shape[1]
 patch_height = patches_imgs_train.shape[2]
 patch_width = patches_imgs_train.shape[3]
-model = get_unet(n_ch, patch_height, patch_width)  #the U-net model
-#model = resnet_50(n_ch, patch_height, patch_width)  #the ResNet model
+#model = get_unet(n_ch, patch_height, patch_width)  #the U-net model
+model = resnet_50(n_ch, patch_height, patch_width)  #the ResNet model
 print("Check: final output of the network:")
 print(model.output_shape)
 plot(model, to_file='./'+name_experiment+'/'+name_experiment + '_model.png')   #check how the model looks like
