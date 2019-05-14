Fix normalisation

src/retinaNN_training.py
@@ -62,7 +62,7 @@ patches_imgs_samples, patches_gts_samples = load_images_labels(
     batch_size,
     N_subimgs
 )
-patches_imgs_samples = (patches_imgs_samples[0:20] + 3) * 85
+patches_imgs_samples = patches_imgs_samples[0:20] * 85. + 127.5
 patches_gts_samples = tf.cast(patches_gts_samples[0:20] * 255., tf.float32)
 patches_gts_samples = tf.reshape(
     patches_gts_samples,