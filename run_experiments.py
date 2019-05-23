b2df1f55990c80888b9fef9db6837ec24b50e0d6
jannes <jannes.hp@t-online.de>
2019-05-23 19:39:56 +0200

    fix n_subimgs

--- run_experiments.py
+++ run_experiments.py
@@ -5,8 +5,11 @@ import os
 global_config = configparser.RawConfigParser()
 global_config.read('./global_config.txt')
 
-subimgs_per_dim = int(global_config.get('global', 'subimgs_per_dim'))
-subimgs_per_img = subimgs_per_dim**2
+
+stats_parser = configparser.RawConfigParser()
+stats_parser.read('./DRIVE_datasets/stats_train.txt')
+
+subimgs_per_img = stats_parser.get('statistics', 'subimages_per_image')
 
 DRIVE_imgs_train = int(global_config.get('DRIVE', 'N_imgs_train'))
 DRIVE_imgs_test = int(global_config.get('DRIVE', 'N_imgs_test'))
@@ -14,14 +17,12 @@ DRIVE_imgs_test = int(global_config.get('DRIVE', 'N_imgs_test'))
 DRIVE_subimgs = subimgs_per_img * DRIVE_imgs_train
 DRIVE_testimgs = subimgs_per_img * DRIVE_imgs_test
 
-
 Synth_imgs_train = int(global_config.get('Synth', 'N_imgs_train'))
 Synth_imgs_test = int(global_config.get('Synth', 'N_imgs_test'))
 
 Synth_subimgs = subimgs_per_img * Synth_imgs_train
 Synth_testimgs = subimgs_per_img * Synth_imgs_test
 
-subimgs_per_img = subimgs_per_dim**2
 imgs_to_visualize = global_config.get('global', 'imgs_to_visualize')
 
 # first is train second test
